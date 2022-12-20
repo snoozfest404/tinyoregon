af25989775e70a0f84f64df53ae2cf4e09b8c6fc
snoozfest <99345180+snoozfest404@users.noreply.github.com>
2022-12-20 14:16:51 +0100

    Refactor Settler constructors

--- src/settler/settler.cpp
+++ src/settler/settler.cpp
@@ -1,9 +1,17 @@
 #include "settler.hpp"
+#include "../entity/entity.hpp"
 
 using namespace settler;
 
-Settler::Settler(const string &name, unsigned int max_health) {
+Settler::Settler(
+    const string &name,
+    unsigned int max_health
+) : Settler(name, max_health, max_health) {}
+
+Settler::Settler(
+    const string &name,
+    unsigned int max_health,
+    unsigned int health
+) : Entity(max_health, health) {
     this->name = name;
-    this->max_health = max_health;
-    this->health = max_health;
 }
--- src/settler/settler.hpp
+++ src/settler/settler.hpp
@@ -12,6 +12,7 @@ namespace settler {
         string name;
     public:
         Settler(const string &name, unsigned int max_health = 100);
+        Settler(const string &name, unsigned int max_health, unsigned int health);
         string get_name() const { return this->name; };
     };
 }
