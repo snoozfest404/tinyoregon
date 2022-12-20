b245a85a1e77a5e9b2aa5983b6861737f0b6b422
snoozfest <99345180+snoozfest404@users.noreply.github.com>
2022-12-20 18:15:35 +0100

    Add copy logic for Caravan class

--- src/settler/caravan.cpp
+++ src/settler/caravan.cpp
@@ -20,3 +20,19 @@ Caravan::Caravan(
     const vector<Settler> &settlers,
     unsigned int max_health
 ) : Caravan(settlers, max_health, max_health) {}
+
+Caravan::Caravan(
+    const Caravan &c
+) : Entity(c.max_health, c.health) {
+    this->settlers = c.settlers;
+    this->alive = c.alive;
+}
+
+Caravan & Caravan::operator=(const Caravan &c) {
+    if (this == &c) {
+        return *this;
+    }
+
+    *this = c;
+    return *this;
+}
--- src/settler/caravan.hpp
+++ src/settler/caravan.hpp
@@ -14,6 +14,8 @@ namespace settler {
     public:
         Caravan(const vector<Settler> &settlers, unsigned int max_health = 100);
         Caravan(const vector<Settler> &settlers, unsigned int max_health, unsigned int health);
+        Caravan(const Caravan &c);
+        Caravan & operator=(const Caravan &c);
         const vector<Settler> & get_settlers() const { return this->settlers; };
     };
 }
