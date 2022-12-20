a908b93215046371d9490315ad07e9e5d4e98132
snoozfest <99345180+snoozfest404@users.noreply.github.com>
2022-12-20 15:50:42 +0100

    Add option to give names to starting settlers

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "item/item.hpp"
 #include "game/game.hpp"
+#include "settler/settler.hpp"
+#include "settler/caravan.hpp"
 
 #include <string>
 #include <iostream>
@@ -12,6 +14,7 @@
 #include <vector>
 #include <regex>
 #include <chrono>
+#include <random>
 
 #define ITEMS_FILE          "items.json"
 #define EVENTS_FILE         "events.json"
@@ -62,6 +65,12 @@ int main(int argc, char **argv)
     while(getline(settler_names_f, line)) {
         settler_name_pool.push_back(line);
     }
+
+    // Used for the embark sequence.
+    // Must remain unaffected by the seed given by the player.
+    std::default_random_engine rand_engine;
+    rand_engine.seed(std::chrono::system_clock::now()
+        .time_since_epoch().count());
 
     fmt::print(fg(fmt::terminal_color::green), "Welcome to the trail!\n");
 
@@ -150,6 +159,42 @@ int main(int argc, char **argv)
         }
         answer_invalid = true;
     }
+
+    answer_invalid = false;
+    vector<settler::Settler> settlers;
+    while (true && (settlers.size() != settler_count)) {
+        fmt::print("{}\n", DIVIDER);
+        fmt::print("What is the name of the {}. settler? (default: <random>)\n",
+            settlers.size()+1);
+        fmt::print("\n");
+
+        if (answer_invalid) {
+            fmt::print(fg(fmt::terminal_color::yellow),
+                "Please provide a valid name.\n");
+        }
+
+        string input;
+        getline(std::cin, input);
+
+        if (input.length() == 0) {
+            std::uniform_int_distribution<int> dist(0, settler_name_pool.size()-1);
+            int idx = dist(rand_engine);
+            fmt::print("IDX {}", idx);
+            string name = settler_name_pool[idx];
+            settlers.push_back(settler::Settler(name));
+            continue;
+        }
+
+        std::smatch res;
+        if (std::regex_match(input, res, rgx(R"(^[a-zA-Z0-9_]+$)"))) {
+            string name = res.str(0);
+            settlers.push_back(settler::Settler(name));
+            continue;
+        }
+        answer_invalid = true;
+    }
+
+    settler::Caravan caravan_instance(settlers);
 
     game::Game game_instance(difficulty, item_pool, settler_name_pool, seed);
 
